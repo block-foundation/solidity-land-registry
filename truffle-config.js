f1fca712943d911ab868b0c26d46596a328fe4b7
Lars van Vianen <lars@vianen.com>
2023-07-27 17:24:53 +0200

    Create truffle-config.js

---
+++ truffle-config.js
@@ -0,0 +1,41 @@
+require("dotenv").config({ path: "./.env" })
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+
+// const fs = require('fs');
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 7545, // Standard Ethereum port (default: none)
+      network_id: "5777", // Any network (default: none)
+    },
+    bsc: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          "wss://data-seed-prebsc-1-s1.binance.org:8545"
+        ),
+      gas: 9900000,
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.8.6", // Fetch exact version from solc-bin (default: truffle's version)
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
+    },
+  },
+  chai: {
+    enableTimeouts: false,
+  },
+}
